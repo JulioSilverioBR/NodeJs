5eb6c987b4d6428028199fa22cfb29b7c1d105b7
Julio Cezar Silvério P <44592638+JulioSilverioBR@users.noreply.github.com>
2020-03-19 01:05:53 -0300

    Att Ejs 19/03;2020

--- app.js
+++ app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var app = express();
+var express = require('express'); // Chamando o express
+var app = express(); // usando o metodo
+
+app.set('view engine', 'ejs'); // Mostrar express que  o EJS é o editor de view
+
+app.get('/tecnologia', function(req, res){ // Trazendo resposta no servidor
+    res.render("sessao/tecnologia");
+});
 
 app.get('/', function(req, res){
-    res.send("<htm><body>Portal de Noticias</body></html>");
+    res.send("<htm><body>Noticias Gerais</body></html>");
 });
 
-app.get('/tecnologia', function(req, res){
-    res.send("<htm><body>Noticias de Tecnologia</body></html>");
-});
-
-app.listen(3000,function(){
+app.listen(3000,function(){ // Chamar servidor na porta 3000
     console.log("Servidor Rodando com Express");
 });
